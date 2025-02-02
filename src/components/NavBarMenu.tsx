8d5628f42e42cd01cdad75944b24b96ec2e651ee
2102kh <144224944+2102kh@users.noreply.github.com>
2025-02-02 16:22:10 +0100

    :art:Added Logo

--- src/components/NavBarMenu.tsx
+++ src/components/NavBarMenu.tsx
@@ -4,6 +4,7 @@ import { CgClose } from "react-icons/cg";
 //import { RiMenu2Line } from "react-icons/ri";
 import { FiAlignRight } from "react-icons/fi";
 import { useState } from 'react';
+import logo from '../assets/logo.png';
 
 const NavBarMenu = () => {
     const [isResponsive, setIsResponsive] = useState(false);
@@ -13,7 +14,9 @@ const NavBarMenu = () => {
 
     return (
         <nav>
-            <NavLink to={'/'} className='logo'>AutoCashDeal</NavLink>
+            <NavLink  to={'/'} className='logo'>
+                <img src={logo} alt="logo" />
+            </NavLink>
             <ul className={`menu-list ${isResponsive ? 'active' : ""}`}>
                 <li>
                     <NavLink to={'/about'}>About Company</NavLink>

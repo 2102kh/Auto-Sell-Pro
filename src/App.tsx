d7bd43a975d0474d4a1966ad2b191ee0e19f10f0
2102kh <144224944+2102kh@users.noreply.github.com>
2025-02-02 14:03:55 +0100

    :hamburger: Added responsive navigation menu for mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import About from './components/About'
+import HomePage from './components/HomePage'
 import NavBarMenu from './components/NavBarMenu'
+import Services from './components/Services'
+import Contact from './components/Contact'
+import './styles/main.scss';
 
 function App() {
  
 
   return (
     <>
-      <div>Wellcome to AutoCashDeal</div>
+      
       <NavBarMenu/>
+      <div>Wellcome to AutoCashDeal</div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   )
 }
--- src/components/NavBarMenu.tsx
+++ src/components/NavBarMenu.tsx
@@ -1,25 +1,39 @@
-
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import '../styles/NavBar.scss';
+import { CgClose } from "react-icons/cg";
+//import { RiMenu2Line } from "react-icons/ri";
+import { FiAlignRight } from "react-icons/fi";
+import { useState } from 'react';
 
 const NavBarMenu = () => {
-  return (
-    <nav>
-        <ul>
-            <li>
-                <Link to={''}>About Company</Link>
-            </li>
-            <li>
-                <Link to={''}>Our Services</Link>
-            </li>
-            <li>
-                <Link to={''}>Contact</Link>
-            </li>
-            <li>
-                <Link to={''}>Auto</Link>
-            </li>
-        </ul>
-    </nav>
-  )
+    const [isResponsive, setIsResponsive] = useState(false);
+    const toggleNavbar = () => {
+        setIsResponsive(!isResponsive);
+    }
+
+    return (
+        <nav>
+            <NavLink to={'/'} className='logo'>AutoCashDeal</NavLink>
+            <ul className={`menu-list ${isResponsive ? 'active' : ""}`}>
+                <li>
+                    <NavLink to={'/about'}>About Company</NavLink>
+                </li>
+                <li>
+                    <NavLink to={'/services'}>Our Services</NavLink>
+                </li>
+                <li>
+                    <NavLink to={'/contact'}>Contact</NavLink>
+                </li>
+                <li>
+                    <NavLink to={''}>Auto</NavLink>
+                </li>
+            </ul>
+            <div className="menu-icon" onClick={toggleNavbar}>
+                {isResponsive ? <CgClose /> : <FiAlignRight />}
+
+            </div>
+        </nav>
+    )
 }
 
 export default NavBarMenu
